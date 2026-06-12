80dd3fc8cd336ab1498e907940b034f0702acdea
Itamar Shaked <Itamar@shaked.in>
2026-06-12 17:15:06 +0300

    Add Alembic baseline migration

---
+++ app/migrations/versions/1fe4caf55858_baseline_existing_schema.py
@@ -0,0 +1,24 @@
+"""Baseline existing schema
+
+Revision ID: 1fe4caf55858
+Revises: 
+Create Date: 2026-06-12 14:14:57.793383
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1fe4caf55858'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
